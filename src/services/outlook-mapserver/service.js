5c7c0c645aa2643d1f3ebeee3291fc7d94c7b09c
Ephraim <ephraim@modevx.com>
2024-05-07 00:00:27 -0500

    explicitly name service funcs

--- src/services/outlook-mapserver/service.js
+++ src/services/outlook-mapserver/service.js
@@ -7,27 +7,27 @@ const convectiveOutlookHTTPClient = createHTTPClient({
     "https://mapservices.weather.noaa.gov/vector/rest/services/outlooks/SPC_wx_outlks/MapServer",
 });
 
-const fetchOutlookLayerById = async (layerId) => {
+const fetchConvectiveOutlookMapServerLayerById = async (layerId) => {
   try {
     const response = await convectiveOutlookHTTPClient.get(
       `/${layerId}/query?f=geojson&geometry=true&outfields=*`
     );
     return response.data.features;
   } catch (error) {
-    console.log(">> fetchLayerGeoJSON:\n", error);
+    // console.log(">> fetchLayerGeoJSON:\n", error);
   }
 };
 
 export const useCategoricalOutlookByLayerId = (layerId) => {
   return useQuery(
     ["convective outlooks", "categorical", `layer ${layerId}`],
-    () => fetchOutlookLayerById(layerId)
+    () => fetchConvectiveOutlookMapServerLayerById(layerId)
   );
 };
 
 export const useProbabilisticOutlookByLayerId = (layerId) => {
   return useQuery(
     ["convective outlooks", "probabilistic", `layer ${layerId}`],
-    () => fetchOutlookLayerById(layerId)
+    () => fetchConvectiveOutlookMapServerLayerById(layerId)
   );
 };
