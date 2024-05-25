0fcdc7e9bb0b3fd27dcc8d60b19defcac268546d
Ephraim <ephraim@modevx.com>
2024-05-24 21:15:15 -0500

    fix color value format

--- src/features/convective-outlooks/_constants/outlook-feature-details.js
+++ src/features/convective-outlooks/_constants/outlook-feature-details.js
@@ -64,38 +64,38 @@ export const CATEGORICAL = Object.freeze({
 export const PROBABILISTIC = Object.freeze({
   2: {
     label: "2%",
-    color: "rgba(56, 168, 0, 255)",
+    color: "rgb(56, 168, 0)",
   },
   5: {
     label: "5%",
-    color: "rgba(111, 25, 3, 255)",
+    color: "rgb(111, 25, 3)",
   },
   10: {
     label: "10%",
-    color: "rgba(255, 198, 0, 255)",
+    color: "rgb(255, 198, 0)",
   },
   15: {
     label: "15%",
-    color: "rgba(230, 0, 0, 255)",
+    color: "rgb(230, 0, 0)",
   },
   30: {
     label: "30%",
-    color: "rgba(250, 0, 255, 255)",
+    color: "rgb(250, 0, 255)",
   },
   45: {
     label: "45%",
-    color: "rgba(119, 6, 244, 255)",
+    color: "rgb(119, 6, 244)",
   },
   60: {
     label: "60%",
-    color: "rgba(0, 77, 168, 255)",
+    color: "rgb(0, 77, 168)",
   },
 });
 
 export const SIGNIFICANT = Object.freeze({
   10: {
     label: "Significant",
-    color: "rgba(212, 208, 200, 0)",
+    color: "rgb(212, 208, 200)",
   },
 });
 
@@ -104,78 +104,78 @@ export const SIGNIFICANT = Object.freeze({
 const CAT_OUTLOOK = Object.freeze({
   2: {
     label: "Thunderstorm",
-    fill: rgba(189, 255, 189, 255),
+    fill: "rgb(189, 255, 189)",
   },
   3: {
     label: "Marginal",
-    fill: rgba(115, 178, 115, 255),
+    fill: "rgb(115, 178, 115)",
   },
   4: {
     label: "Slight",
-    fill: rgba(247, 247, 143, 255),
+    fill: "rgb(247, 247, 143)",
   },
   5: {
     label: "Enhanced",
-    fill: rgba(230, 152, 0, 255),
+    fill: "rgb(230, 152, 0)",
   },
   6: {
     label: "Moderate",
-    fill: rgba(255, 0, 0, 255),
+    fill: "rgb(255, 0, 0)",
   },
   8: {
     label: "High",
-    fill: rgba(255, 0, 197, 255),
+    fill: "rgb(255, 0, 197)",
   },
 });
 const PROB_TORNADO = Object.freeze({
   2: {
     label: "2%",
-    fill: rgba(56, 168, 0, 255),
+    fill: "rgb(56, 168, 0)",
   },
   5: {
     label: "5%",
-    fill: rgba(111, 25, 3, 255),
+    fill: "rgb(111, 25, 3)",
   },
   10: {
     label: "10%",
-    fill: rgba(255, 198, 0, 255),
+    fill: "rgb(255, 198, 0)",
   },
   15: {
     label: "15%",
-    fill: rgba(230, 0, 0, 255),
+    fill: "rgb(230, 0, 0)",
   },
   30: {
     label: "30%",
-    fill: rgba(250, 0, 255, 255),
+    fill: "rgb(250, 0, 255)",
   },
   45: {
     label: "45%",
-    fill: rgba(119, 6, 244, 255),
+    fill: "rgb(119, 6, 244)",
   },
   60: {
     label: "60%",
-    fill: rgba(0, 77, 168, 255),
+    fill: "rgb(0, 77, 168)",
   },
 });
 const PROB_WIND_HAIL = Object.freeze({
   5: {
     label: "5%",
-    fill: rgba(198, 162, 148, 255),
+    fill: "rgb(198, 162, 148)",
   },
   15: {
     label: "15%",
-    fill: rgba(255, 255, 0, 255),
+    fill: "rgb(255, 255, 0)",
   },
   30: {
     label: "30%",
-    fill: rgba(255, 0, 0, 255),
+    fill: "rgb(255, 0, 0)",
   },
   45: {
     label: "45%",
-    fill: rgba(255, 0, 197, 255),
+    fill: "rgb(255, 0, 197)",
   },
   60: {
     label: "60%",
-    fill: rgba(168, 0, 132, 255),
+    fill: "rgb(168, 0, 132)",
   },
 });
