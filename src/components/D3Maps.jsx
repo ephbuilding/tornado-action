6a929f080a5d6aedcbc75f2c5377cf3df98b9c32
Ephraim <ephraim@modevx.com>
2024-05-28 00:26:11 -0500

    create single file for D3 maps

---
+++ src/components/D3Maps.jsx
@@ -0,0 +1,58 @@
+import * as TopoJSONClient from "topojson-client";
+import AlbersTopoJSONMap from "json/topojson-albers-map.json";
+
+const meshedCountyFeatures = TopoJSONClient.mesh(
+  AlbersTopoJSONMap,
+  AlbersTopoJSONMap.objects.counties
+  // (a, b) => a !== b
+);
+
+// console.log(">> MESHED COUNTY FEATURES >>\n", meshedCountyFeatures);
+
+export const USCountyMap = ({ children, pathGen }) => {
+  return (
+    <svg viewBox="0 0 975 610" xmlns="http://www.w3.org/2000/svg">
+      <CountyFeatures pathGen={pathGen} />
+
+      {children}
+    </svg>
+  );
+};
+export const USMapLoading = ({ loadingMessage }) => {
+  return (
+    <USStateMap>
+      <text x={350} y={250} fill="red" className="text-3xl font-bold">
+        {loadingMessage}
+      </text>
+    </USStateMap>
+  );
+};
+export const USStateMap = ({ children }) => {
+  const states = TopoJSONClient.feature(AlbersTopoJSONMap, "states");
+
+  // if (states) console.log("states >>\n", states);
+
+  return (
+    <svg viewBox="0 -60 975 610" xmlns="http://www.w3.org/2000/svg">
+      <path d={albersGeoPath(states)} stroke="white" fill="grey" />
+
+      {children}
+    </svg>
+  );
+};
+
+// SUB-COMPONENTS
+const CountyFeatures = ({ pathGen }) => {
+  const { features: countyFeatures } = TopoJSONClient.feature(
+    AlbersTopoJSONMap,
+    "counties"
+  );
+
+  return (
+    <path
+      d={pathGen({ type: "FeatureCollection", features: countyFeatures })}
+      stroke="white"
+      fill="grey"
+    />
+  );
+};
