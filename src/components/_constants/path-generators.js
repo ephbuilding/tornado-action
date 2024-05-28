199baf5338428b23c4a38ea215b8cf05d8f86dc8
Ephraim <ephraim@modevx.com>
2024-05-28 00:23:58 -0500

    relo constants to constants/

--- src/components/_constants/path-generators.js
+++
@@ -1,7 +0,0 @@
-import rewind from "@turf/rewind";
-import { geoAlbers, geoPath } from "d3-geo";
-
-export const pathGenerator = geoPath(geoAlbers());
-
-export const rewindPathGenerator = (feature) =>
-	pathGenerator(rewind(feature, { reverse: true }));
