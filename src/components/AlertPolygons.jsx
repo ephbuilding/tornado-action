471dd3fbbb36b98983717eb0423d8a11a8ec70de
Ephraim <ephraim@modevx.com>
2024-05-27 22:35:10 -0500

    create AlertPolygons component file

---
+++ src/components/AlertPolygons.jsx
@@ -0,0 +1,58 @@
+import {
+  alertIsDestructiveStorm,
+  alertIsPDS,
+  alertIsTornadoEmergency,
+} from "utils/nws-alerts";
+import { reverseAlbersGeoPath } from "utils/geometry";
+import { NWS_ALERT_COLORS } from "constants/nws-alerts";
+import { createWatchAlertGeometry } from "utils/geometry";
+
+export const WarningPolygon = ({ alert, color, onClickCallback }) => {
+  const isDestructiveStorm = alertIsDestructiveStorm(alert);
+  const isPDS = alertIsPDS(alert);
+  const isTornadoEmergency = alertIsTornadoEmergency(alert);
+  const polygonColor = isTornadoEmergency
+    ? NWS_ALERT_COLORS.tornado_emergency
+    : isPDS
+    ? NWS_ALERT_COLORS.particularly_dangerous_situation
+    : isDestructiveStorm
+    ? NWS_ALERT_COLORS.destructive_storm
+    : color;
+
+  return (
+    alert?.geometry && (
+      <path
+        d={reverseAlbersGeoPath(alert.geometry)}
+        fill={polygonColor}
+        stroke={polygonColor}
+        fillOpacity={0.65}
+        strokeOpacity={0.85}
+        strokeWidth={1}
+        onClick={() => onClickCallback(alert)}
+      />
+    )
+  );
+};
+
+export const WatchPolygon = ({ alert, color, onClickCallback }) => {
+  const isPDS = alertIsPDS(alert);
+  const isDestructiveStorm = alertIsDestructiveStorm(alert);
+  const polygonColor = isPDS
+    ? NWS_ALERT_COLORS.particularly_dangerous_situation
+    : isDestructiveStorm
+    ? NWS_ALERT_COLORS.destructive_storm
+    : color;
+  const watchGeometry = createWatchAlertGeometry(alert);
+
+  return (
+    <path
+      d={reverseAlbersGeoPath(watchGeometry)}
+      fill={polygonColor}
+      stroke={polygonColor}
+      fillOpacity={0.5}
+      strokeOpacity={0.75}
+      strokeWidth={0.5}
+      onClick={() => onClickCallback(alert)}
+    />
+  );
+};
