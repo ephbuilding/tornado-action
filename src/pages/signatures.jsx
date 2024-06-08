74096d7bbacc2cd9c685897ccff522fda2b7c064
Ephraim <ephraim@modevx.com>
2024-06-08 12:03:41 -0500

    create SignaturePoint component that returns size-adjustable svg circle

--- src/pages/signatures.jsx
+++ src/pages/signatures.jsx
@@ -19,37 +19,10 @@ const SignaturesScreen = () => {
     "2021121101:2021121102"
   );
 
-  if (tornadoSigs) {
-    tornadoSigs.forEach((signature) => {
-      // console.log(parsePointLatLong(signature.SHAPE));
-      console.log("Signature >> ", parsePointLatLong(signature));
-    });
-    // console.log(tornadoSigs);
-    // const point = tornadoSigs[0].SHAPE;
-    // const [lat, lon] = point.slice(7, -1).split(" ");
-    // console.log("POINT: ", parseFloat(lat), parseFloat(lon));
-  }
-  if (hailSigs) {
-    // console.log(hailSigs);
-    const point = hailSigs[0].SHAPE;
-    const [lat, lon] = point.slice(7, -1).split(" ");
-    console.log("POINT: ", parseFloat(lat), parseFloat(lon));
-  }
-  // if (mesoSigs) {
-  //   // console.log(mesoSigs);
-  //   const point = mesoSigs[0]?.SHAPE;
-  //   const [lat, lon] = point?.slice(7, -1).split(" ");
-  //   console.log("POINT: ", parseFloat(lat), parseFloat(lon));
-  // }
-
-  // console.log(
-  //   "POINT (-89.4185046707378 40.0321740537808)".slice(7, -1).split(" ")
-  // );
-
   return (
     <PageLayout>
-      <div className="grid grid-cols-3">
-        <div className="text-white">
+      <div className="grid grid-cols-2">
+        {/* <div className="text-white">
           {tornadoSigs &&
             Object.keys(tornadoSigs[0]).map((key) => (
               <div key={key}>
@@ -57,8 +30,8 @@ const SignaturesScreen = () => {
                 <span>{tornadoSigs[0][key]}</span>
               </div>
             ))}
-        </div>
-        <div className="text-white">
+        </div> */}
+        {/* <div className="text-white">
           {hailSigs &&
             Object.keys(hailSigs[0]).map((key) => (
               <div key={key}>
@@ -66,7 +39,7 @@ const SignaturesScreen = () => {
                 <span>{hailSigs[0][key]}</span>
               </div>
             ))}
-        </div>
+        </div> */}
         {/* <div className="text-white">
           {mesoSigs &&
             Object.keys(mesoSigs[0]).map((key) => (
@@ -77,52 +50,55 @@ const SignaturesScreen = () => {
             ))}
         </div> */}
       </div>
-      <USStateMap>
-        <g>
-          {tornadoSigs &&
-            tornadoSigs.map((signature) => {
-              const { WSR_ID, CELL_ID, ZTIME } = signature;
-              return (
-                <path
-                  key={`${WSR_ID}-${CELL_ID}-${ZTIME}`}
-                  d={reverseAlbersGeoPath({
-                    type: "Point",
-                    coordinates: parsePointLatLong(signature),
-                  })}
-                  fill="red"
-                  stroke="black"
-                />
-              );
-            })}
-        </g>
-      </USStateMap>
-      <USStateMap>
-        <g>
-          {hailSigs &&
-            hailSigs.map((signature) => {
-              const { WSR_ID, CELL_ID, ZTIME } = signature;
-              return (
-                <path
-                  key={`${WSR_ID}-${CELL_ID}-${ZTIME}`}
-                  d={reverseAlbersGeoPath({
-                    type: "Point",
-                    coordinates: parsePointLatLong(signature),
-                  })}
-                  fill="blue"
-                  stroke="black"
-                />
-              );
-            })}
-        </g>
-      </USStateMap>
+      <div className="grid grid-cols-2">
+        <USStateMap>
+          <g>
+            {tornadoSigs &&
+              tornadoSigs.map((signature) => {
+                return (
+                  <SignaturePoint
+                    key={signature.POINT}
+                    signature={signature}
+                    color="red"
+                  />
+                );
+              })}
+          </g>
+        </USStateMap>
+        <USStateMap>
+          <g>
+            {hailSigs &&
+              hailSigs.map((signature) => {
+                return (
+                  <SignaturePoint
+                    key={signature.POINT}
+                    signature={signature}
+                    color="blue"
+                  />
+                );
+              })}
+          </g>
+        </USStateMap>
+      </div>
     </PageLayout>
   );
 };
 
 export default SignaturesScreen;
 
-const parsePointLatLong = (signature) => {
+// --- UTILS
+const parseSignatureLatLon = (signature) => {
   const point = signature.SHAPE;
   const [lat, lon] = point.slice(7, -1).split(" ");
   return [parseFloat(lat), parseFloat(lon)];
+};
+// --- SUB-COMPONENTS
+const SignaturePoint = ({ signature, color }) => {
+  const [lat, lon] = parseSignatureLatLon(signature);
+  const [x, y] = albersGeoPath.centroid({
+    type: "Point",
+    coordinates: [lat, lon],
+  });
+
+  return <circle cx={x} cy={y} r={3} fill={color} stroke="black" />;
 };
