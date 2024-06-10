6d00bdd3d9b106c42dba1e1f9375ee04a16e5fa9
Ephraim <ephraim@modevx.com>
2024-06-10 16:20:45 -0500

    refactor point sub-components into single SignaturePoint adding color & radius props

--- src/pages/signatures.jsx
+++ src/pages/signatures.jsx
@@ -12,9 +12,42 @@ const SignaturesScreen = () => {
   const { data: tornadoSigs } =
     useTornadoSignaturesByDateRange("20130531:20130601");
   const { data: hailSigs } = useHailSignaturesByDateRange("20130531:20130601");
+  // ? --> why no meso signature responses from SWDI?
   const { data: mesoSigs } = useMesoSignaturesByDateRange(
     "2021121101:2021121102"
   );
+  let tvsByMxdv = {};
+  let hailByMaxSize = {};
+
+  if (tornadoSigs) {
+    tvsByMxdv._0_50 = tornadoSigs.filter(({ MXDV }) => +MXDV <= 50);
+    tvsByMxdv._50_100 = tornadoSigs.filter(
+      ({ MXDV }) => +MXDV > 50 && +MXDV <= 100
+    );
+    tvsByMxdv._100_150 = tornadoSigs.filter(
+      ({ MXDV }) => +MXDV > 100 && +MXDV <= 150
+    );
+    tvsByMxdv._150_200 = tornadoSigs.filter(
+      ({ MXDV }) => +MXDV > 150 && +MXDV <= 200
+    );
+    tvsByMxdv._201 = tornadoSigs.filter(({ MXDV }) => +MXDV > 200);
+  }
+  if (hailSigs) {
+    hailByMaxSize._0_1 = hailSigs.filter(({ MAXSIZE }) => +MAXSIZE <= 1);
+    hailByMaxSize._1_2 = hailSigs.filter(
+      ({ MAXSIZE }) => +MAXSIZE > 1 && +MAXSIZE <= 2
+    );
+    hailByMaxSize._2_3 = hailSigs.filter(
+      ({ MAXSIZE }) => +MAXSIZE > 2 && +MAXSIZE <= 3
+    );
+    hailByMaxSize._3_4 = hailSigs.filter(
+      ({ MAXSIZE }) => +MAXSIZE > 3 && +MAXSIZE <= 4
+    );
+    hailByMaxSize._4_5 = hailSigs.filter(
+      ({ MAXSIZE }) => +MAXSIZE > 4 && +MAXSIZE <= 5
+    );
+    hailByMaxSize._5 = hailSigs.filter(({ MAXSIZE }) => +MAXSIZE > 5);
+  }
 
   return (
     <PageLayout>
@@ -22,33 +55,129 @@ const SignaturesScreen = () => {
         <div>
           <h2 className="text-center">Tornado Vortex Signatures</h2>
           <USStateMap>
-            <g>
-              {tornadoSigs &&
-                tornadoSigs.map((signature) => {
+            {tornadoSigs && (
+              <g>
+                {tvsByMxdv._0_50.map((signature) => {
                   return (
-                    <TVSSignaturePoint
+                    <SignaturePoint
                       key={signature.POINT}
                       signature={signature}
+                      color="green"
+                      radius={2}
                     />
                   );
                 })}
-            </g>
+                {tvsByMxdv._50_100.map((signature) => {
+                  return (
+                    <SignaturePoint
+                      key={signature.POINT}
+                      signature={signature}
+                      color="blue"
+                      radius={4}
+                    />
+                  );
+                })}
+                {tvsByMxdv._100_150.map((signature) => {
+                  return (
+                    <SignaturePoint
+                      key={signature.POINT}
+                      signature={signature}
+                      color="yellow"
+                      radius={6}
+                    />
+                  );
+                })}
+                {tvsByMxdv._150_200.map((signature) => {
+                  return (
+                    <SignaturePoint
+                      key={signature.POINT}
+                      signature={signature}
+                      color="orange"
+                      radius={8}
+                    />
+                  );
+                })}
+                {tvsByMxdv._201.map((signature) => {
+                  return (
+                    <SignaturePoint
+                      key={signature.POINT}
+                      signature={signature}
+                      color="red"
+                      radius={10}
+                    />
+                  );
+                })}
+              </g>
+            )}
           </USStateMap>
         </div>
         <div>
           <h2 className="text-center">Hail Signatures</h2>
           <USStateMap>
-            <g>
-              {hailSigs &&
-                hailSigs.map((signature) => {
+            {hailSigs && (
+              <g>
+                {hailByMaxSize._0_1.map((signature) => {
                   return (
-                    <HailSignaturePoint
+                    <SignaturePoint
                       key={signature.POINT}
                       signature={signature}
+                      color="green"
+                      radius={3}
                     />
                   );
                 })}
-            </g>
+                {hailByMaxSize._1_2.map((signature) => {
+                  return (
+                    <SignaturePoint
+                      key={signature.POINT}
+                      signature={signature}
+                      color="blue"
+                      radius={4.5}
+                    />
+                  );
+                })}
+                {hailByMaxSize._2_3.map((signature) => {
+                  return (
+                    <SignaturePoint
+                      key={signature.POINT}
+                      signature={signature}
+                      color="yellow"
+                      radius={6}
+                    />
+                  );
+                })}
+                {hailByMaxSize._3_4.map((signature) => {
+                  return (
+                    <SignaturePoint
+                      key={signature.POINT}
+                      signature={signature}
+                      color="orange"
+                      radius={7.5}
+                    />
+                  );
+                })}
+                {hailByMaxSize._4_5.map((signature) => {
+                  return (
+                    <SignaturePoint
+                      key={signature.POINT}
+                      signature={signature}
+                      color="red"
+                      radius={9}
+                    />
+                  );
+                })}
+                {hailByMaxSize._5.map((signature) => {
+                  return (
+                    <SignaturePoint
+                      key={signature.POINT}
+                      signature={signature}
+                      color="pink"
+                      radius={10}
+                    />
+                  );
+                })}
+              </g>
+            )}
           </USStateMap>
         </div>
       </div>
@@ -64,60 +193,25 @@ const parseSignatureLatLon = (signature) => {
   const [lat, lon] = point.slice(7, -1).split(" ");
   return [parseFloat(lat), parseFloat(lon)];
 };
-// --- SUB-COMPONENTS
-const TVSSignaturePoint = ({ signature }) => {
+const convertSignaturePointToD3Centroid = (signature) => {
   const [lat, lon] = parseSignatureLatLon(signature);
-  const [x, y] = albersGeoPath.centroid({
+  return albersGeoPath.centroid({
     type: "Point",
     coordinates: [lat, lon],
   });
-  const mxdv = parseInt(signature.MXDV);
-  let mxdvColor;
-  let pointRadius;
-
-  if (mxdv <= 50) {
-    mxdvColor = "green";
-    pointRadius = 2;
-  } else if (mxdv <= 100) {
-    mxdvColor = "blue";
-    pointRadius = 4;
-  } else if (mxdv <= 150) {
-    mxdvColor = "yellow";
-    pointRadius = 6;
-  } else if (mxdv <= 200) {
-    mxdvColor = "orange";
-    pointRadius = 8;
-  } else {
-    mxdvColor = "red";
-    pointRadius = 10;
-  }
+};
+// --- SUB-COMPONENTS
+const SignaturePoint = ({ signature, color, radius }) => {
+  const [x, y] = convertSignaturePointToD3Centroid(signature);
 
   return (
     <circle
       cx={x}
       cy={y}
-      r={pointRadius}
-      fill={mxdvColor}
+      r={radius}
+      fill={color}
       fillOpacity={0.5}
       stroke="black"
     />
   );
-};
-const HailSignaturePoint = ({ signature }) => {
-  const [lat, lon] = parseSignatureLatLon(signature);
-  const [x, y] = albersGeoPath.centroid({
-    type: "Point",
-    coordinates: [lat, lon],
-  });
-  const maxSize = parseInt(signature.MAXSIZE);
-  let maxSizeColor;
-
-  if (maxSize <= 1) maxSizeColor = "green";
-  else if (maxSize <= 2) maxSizeColor = "blue";
-  else if (maxSize <= 3) maxSizeColor = "yellow";
-  else if (maxSize <= 4) maxSizeColor = "orange";
-  else if (maxSize <= 5) maxSizeColor = "red";
-  else maxSizeColor = "pink";
-
-  return <circle cx={x} cy={y} r={4} fill={maxSizeColor} stroke="black" />;
 };
